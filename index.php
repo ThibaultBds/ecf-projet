d635fc1ed1285395df0d93b7f4b39a4b2765b937
ThibaultBds <thibauklt.brandes@gmail.com>
2025-07-21 15:55:23 +0200

    Ajout debug affichage erreurs sur index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Test index.php racine Heroku<br>";
+// Test simple pour vérifier l'exécution
+echo "Debug : index.php lancé<br>";
 
-include __DIR__ . '/pages/index.php';
+// Attention à l'inclusion, adapte le chemin si besoin
+if (file_exists(__DIR__ . '/pages/index.php')) {
+    include __DIR__ . '/pages/index.php';
+} else {
+    echo "pages/index.php introuvable<br>";
+}
