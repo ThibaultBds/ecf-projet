825d3021097ae67f0a338fe2597171424e3a498c
ThibaultBds <thibauklt.brandes@gmail.com>
2025-07-21 17:49:06 +0200

    Résolution des conflits avec Heroku

---
+++ frontend/public/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '229b6a59d23021ab73f7a77bad7a5abdf21ba7ec',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '229b6a59d23021ab73f7a77bad7a5abdf21ba7ec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
