d205bcfcd9bd15b4cb25a0ed43daabdd47d9e198
ThibaultBds <thibauklt.brandes@gmail.com>
2025-07-21 14:41:16 +0200

    Premier commit

--- backend/public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['test'] = 'Ceci est un test';
-echo 'Session créée. <a href="test2.php">Vérifier</a>';
-?>
-
-
-
--- backend/public/test2.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-echo 'Contenu de la session : ';
-print_r($_SESSION);
-?>
-
-
