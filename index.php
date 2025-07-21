2479fc69c423b232d0a6348a2a6b3ce9e76498dc
ThibaultBds <thibauklt.brandes@gmail.com>
2025-07-21 15:54:03 +0200

    Debug affiche erreurs PHP sur index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-header("Location: pages/index.php");
-exit;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Test index.php racine Heroku<br>";
+
+include __DIR__ . '/pages/index.php';
