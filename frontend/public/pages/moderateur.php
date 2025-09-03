b1bdfeacde1bcf4383b6a94052e4b4116aef29c6
Thibault <105049008+ThibaultBds@users.noreply.github.com>
2025-09-03 18:02:27 +0200

    correction du lien stylesheet

--- frontend/public/pages/moderateur.php
+++ frontend/public/pages/moderateur.php
@@ -31,7 +31,7 @@ try {
   <meta charset="UTF-8" />
   <title>Modération - EcoRide</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/ecoride/frontend/public/assets/css/style.css" />
+<link rel="stylesheet" href="../assets/css/style.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
