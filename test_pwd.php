5af13dedce507943d64d0ac1ed6e85ac30a6ec60
Thibault <105049008+ThibaultBds@users.noreply.github.com>
2025-09-05 22:52:38 +0200

    suppression de fichiers test

--- test_pwd.php
+++
@@ -1,4 +0,0 @@
-<?php
-// test_pwd.php (à la racine du projet)
-$hash = '$2y$10$cyUwI2HvMgmOmfxevb5H9epq.NTk4jHSSqXkBMoMJPK6AZP8AcZe6'; // hash de test1234
-var_dump(password_verify('test1234', $hash)); // doit afficher: bool(true)
