25f6f11272e4af78b3f2617119f4eefd758df37f
Thibault <105049008+ThibaultBds@users.noreply.github.com>
2026-02-06 14:46:17 +0100

    Mise en place du MVC avec une route d'accueil fonctionnelle

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Router loaded";
+class Router 
+{
+    public function dispatch() 
+    {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if ($uri === '/' || $uri === '') {
+            require __DIR__ . '/../Controllers/HomeController.php';
+            (new HomeController())->index(); 
+            return; 
+        }
+        echo '404 - Page non trouvée';
+    } 
+}
