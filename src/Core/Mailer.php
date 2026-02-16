5999a975ff0c29ed44e4c9421a051a98f192fc5a
Thibault <105049008+ThibaultBds@users.noreply.github.com>
2026-02-16 09:36:55 +0100

    Ajout MongoDB + extension PHP + setup Docker

---
+++ src/Core/Mailer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Core;
+
+class Mailer
+{
+    public static function send(string $to, string $subject, string $message): bool
+    {
+        $from = @"noreply@ecoride.fr";
+
+        $headers = "From: {@from}\r\n";
+        $headers .= "Reply to : {@from}\r\n";
+        $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        return mail($to, $subject, $message, $headers);
+    }
+}
---
+++ src/Core/MongoDB.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Core;
+
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\Query;
+use MongoDB\Driver\BulkWrite;
+
+class MongoDB
+{
+    private static $instance = null;
+    private $manager;
+    private $database = 'ecoride';
+
+    private function __construct()
+    {
+        $uri = getenv('MONGO_URI') ?: 'mongodb://mongo:27017';
+        $this->manager = new Manager($uri);
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getManager(): Manager
+    {
+        return $this->manager;
+    }
+
+    /**
+     * Find one document
+     */
+    public function findOne(string $collection, array $filter): ?array
+    {
+        $query = new Query($filter, ['limit' => 1]);
+        $cursor = $this->manager->executeQuery("{$this->database}.{$collection}", $query);
+        $results = $cursor->toArray();
+
+        if (empty($results)) {
+            return null;
+        }
+
+        return (array) $results[0];
+    }
+
+    /**
+     * Find multiple documents
+     */
+    public function find(string $collection, array $filter = [], array $options = []): array
+    {
+        $query = new Query($filter, $options);
+        $cursor = $this->manager->executeQuery("{$this->database}.{$collection}", $query);
+
+        $results = [];
+        foreach ($cursor as $doc) {
+            $results[] = (array) $doc;
+        }
+        return $results;
+    }
+
+    /**
+     * Insert one document
+     */
+    public function insertOne(string $collection, array $document): void
+    {
+        $bulk = new BulkWrite();
+        $bulk->insert($document);
+        $this->manager->executeBulkWrite("{$this->database}.{$collection}", $bulk);
+    }
+
+    /**
+     * Update or insert (upsert) a document
+     */
+    public function upsert(string $collection, array $filter, array $data): void
+    {
+        $bulk = new BulkWrite();
+        $bulk->update($filter, ['$set' => $data], ['upsert' => true]);
+        $this->manager->executeBulkWrite("{$this->database}.{$collection}", $bulk);
+    }
+
+    /**
+     * Delete documents
+     */
+    public function delete(string $collection, array $filter): void
+    {
+        $bulk = new BulkWrite();
+        $bulk->delete($filter);
+        $this->manager->executeBulkWrite("{$this->database}.{$collection}", $bulk);
+    }
+}
